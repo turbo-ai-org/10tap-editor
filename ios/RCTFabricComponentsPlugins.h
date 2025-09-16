3cda1f30d165f3bafb463d4d6ac17b58a13c2123
Griffin Huth <41766864+griffinshuth@users.noreply.github.com>
2025-09-16 10:57:23 -0600

    updated the repo for new arch

---
+++ ios/RCTFabricComponentsPlugins.h
@@ -0,0 +1,18 @@
+//
+//  RCTFabricComponentsPlugins.h
+//  TenTapEditor
+//
+//  Created by Claude on 2024-12-28.
+//
+
+#ifdef RCT_NEW_ARCH_ENABLED
+
+#import <React/RCTComponentViewProtocol.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+Class<RCTComponentViewProtocol> TenTapViewCls(void);
+
+NS_ASSUME_NONNULL_END
+
+#endif
