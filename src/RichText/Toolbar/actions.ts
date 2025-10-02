7cbd91ccf126aa869017c24d7b16f057a41f0231
Kartikeye (Tiki) Gupta <kg330@duke.edu>
2025-10-01 18:05:18 -0700

    fix compilation error

--- src/RichText/Toolbar/actions.ts
+++ src/RichText/Toolbar/actions.ts
@@ -1,4 +1,4 @@
-import { Platform, Keyboard } from 'react-native';
+import { Keyboard } from 'react-native';
 import { Images } from '../../assets';
 import { EditorActionType } from '../../types/Actions';
 import type { EditorBridge } from '../../types';
